c606343ae57d84e9e22b2dbde55ad656f8d585e9
katerina500 <ekaterina.paramore@yandex.ru>
2024-05-03 19:12:22 +0400

    +

---
+++ src/ContactForm.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+const ContactForm = () => {
+
+    const[value, setValue] = useState(0);
+
+    
+    const handleSubmit = (e) => {
+    e.preventDefault() 
+    const formData = new FormData(e.currentTarget);
+    console.log(formData);
+    console.log([...formData.entries()]);
+    setValue(value + 1);
+    e.currentTarget.reset();
+
+
+    fetch('http://localhost:3000/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({formData})
+      });
+      
+    }
+
+    
+
+
+    return(
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input id='name'  type="text"
+                name="name"
+                className="form-input"
+                placeholder="Name : " 
+                ></input>
+                <input id='email' type="email"
+                name="email"
+                className="form-input"
+                placeholder="Email : " 
+                ></input>
+                <input id='message' type="text"
+                name="message"
+                className="form-input"
+                placeholder="Message : " 
+                ></input>
+                <button  className="btn" id="btn" type="submit"> Submit </button>
+            </form>
+        </div>
+    )
+};
+
+
+export default ContactForm;
